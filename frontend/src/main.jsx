52c6f3461e7adbeac57c7bdc1cddea4471f5dd9d
Akash Sharma <aakash6263264@gmail.com>
2024-06-08 12:08:37 +0530

    fixing cart section

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+//  http://localhost:8000
 export const Server = "https://trendhive.onrender.com/api/v1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
