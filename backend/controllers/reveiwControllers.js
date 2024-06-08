c9bc79fc956b0110fe6a163e61d8edfde59b304d
Akash Sharma <aakash6263264@gmail.com>
2024-06-08 12:52:12 +0530

    fetching reviews

--- backend/controllers/reveiwControllers.js
+++ backend/controllers/reveiwControllers.js
@@ -8,7 +8,6 @@ const getProductReviews = async (req, res) => {
     const reviews2 = await Review.find({
       product_id: "66612ca87fa2d7f414e56765",
     });
-    // console.log("reviews")
     res.status(200).send({ reviews: reviews ? reviews : reviews2 });
   } catch (err) {
     res.status(500).json({ error: "Failed to fetch reviews" });
